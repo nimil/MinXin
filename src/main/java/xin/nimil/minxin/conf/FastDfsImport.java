5fe0d1dcfc019178b9c96177474e082b5fbd9a5e
nimil <nimilgg@qq.com>
2018-11-28 22:47:22 +0800

    users logic

---
+++ src/main/java/xin/nimil/minxin/conf/FastDfsImport.java
@@ -0,0 +1,19 @@
+package xin.nimil.minxin.conf;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+/**
+ * @Author:nimil e-mail:nimilgg@qq.com
+ * @Date:2018/11/28
+ * @Time:22:38
+ */
+@Configuration
+@Import(FdfsClientConfig.class)
+// 解决jmx重复注册bean的问题
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class FastDfsImport {
+}
