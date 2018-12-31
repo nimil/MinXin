79e850c59724c4505f2db53bb7a423a207b5a51c
nimil <nimilgg@qq.com>
2018-12-31 20:09:11 +0800

    last

---
+++ src/main/java/xin/nimil/minxin/enums/OpratorFriendRequestTypeEnum.java
@@ -0,0 +1,9 @@
+package xin.nimil.minxin.enums;
+
+/**
+ * @Author:nimil e-mail:nimilgg@qq.com
+ * @Date:2018/12/23
+ * @Time:15:09
+ */
+public enum OpratorFriendRequestTypeEnum {
+}
---
+++ src/main/java/xin/nimil/minxin/vo/MyFriendsVO.java
@@ -0,0 +1,9 @@
+package xin.nimil.minxin.vo;
+
+/**
+ * @Author:nimil e-mail:nimilgg@qq.com
+ * @Date:2018/12/31
+ * @Time:19:40
+ */
+public class MyFriendsVO {
+}
